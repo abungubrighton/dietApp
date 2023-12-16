8c8bf6bcf1f6d417c8a3b269339872c7df4c0a2b
abungubrighton <brighton@ibuqacapital.com>
2023-12-16 16:42:58 +0300

    chore:text field search icons

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,31 +8,60 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar(),
+      backgroundColor: Colors.white,
       body: Column(children: [
-        Container(
-          margin: const EdgeInsets.only(top: 40, left: 20, right: 20),
-          decoration: BoxDecoration(boxShadow: [
-            BoxShadow(
-                color: const Color(0xFF1D1617).withOpacity(0.11),
-                blurRadius: 40,
-                spreadRadius: 0.0)
-          ]),
-          child: TextField(
-            decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.white,
-                hintText: 'Search a diet',
-                hintStyle:
-                    const TextStyle(color: Color(0xFFFDDADA), fontSize: 14),
-                contentPadding: const EdgeInsets.all(15),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15),
-                  borderSide: BorderSide.none,
-                )),
-          ),
-        ),
+        _searchField(),
       ]),
     );
+  }
+
+  Container _searchField() {
+    return Container(
+        margin: const EdgeInsets.only(top: 40, left: 20, right: 20),
+        decoration: BoxDecoration(boxShadow: [
+          BoxShadow(
+              color: const Color(0xFF1D1617).withOpacity(0.11),
+              blurRadius: 40,
+              spreadRadius: 0.0)
+        ]),
+        child: TextField(
+          decoration: InputDecoration(
+              prefixIcon: Padding(
+                padding: const EdgeInsets.all(12),
+                child: SvgPicture.asset('assets/icons/Search.svg'),
+              ),
+              suffixIcon: Container(
+                width: 100,
+                child: IntrinsicHeight(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      const VerticalDivider(
+                        color: Colors.black,
+                        indent: 10,
+                        endIndent: 10,
+                        thickness: 0.1,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(12),
+                        child: SvgPicture.asset('assets/icons/Filter.svg'),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              filled: true,
+              fillColor: Colors.white,
+              hintText: 'Search a diet',
+              hintStyle:
+                  const TextStyle(color: Color(0xFFFDDADA), fontSize: 14),
+              contentPadding: const EdgeInsets.all(15),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(15),
+                borderSide: BorderSide.none,
+              )),
+        ),
+      );
   }
 
   AppBar appBar() {
